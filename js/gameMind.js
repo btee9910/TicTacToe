b562b40a79f77abaa58a18105f0d4228880db4ec
btee9910 <btee9910@gmail.com>
2023-04-03 17:03:34 +1000

    initial planning phase

---
+++ js/gameMind.js
@@ -0,0 +1,21 @@
+const data = {
+    player1: '',
+    player2: '',
+    player1token: 'X'
+    player2token: 'O',
+}
+
+
+//get basic function for now
+
+
+$('document').ready( function() {
+
+
+
+
+
+
+
+
+});
